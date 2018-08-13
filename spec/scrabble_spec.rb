87e1f3034cf2ca5c90df96c96f3dc17a83d8069f
Roy Kim <rykim79@gmail.com>
2018-08-13 14:00:37 -0400

    This commit will test scrabble on words that include numbers AND slightly refactor code

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,16 +4,21 @@ require 'scrabble'
 describe 'Word#scrabble' do
   it("returns a scrabble score for a letter") do
     word = Word.new("a")
-    expect(word.scrabble()).to(eq(1))
+    expect(word.scrabble()).to eq(1)
   end
 
   it("returns a scrabble score for a word") do
     word = Word.new("boo")
-    expect(word.scrabble()).to(eq(5))
+    expect(word.scrabble()).to eq(5)
   end
 
-  it("returns a scrabble score for a word") do
-    word = Word.new("oxen")
-    expect(word.scrabble()).to(eq(11))
+  it("returns a scrabble score for a word regardless of case") do
+    word = Word.new("oXen")
+    expect(word.scrabble()).to eq(11)
+  end
+
+  it("returns a scrabble score for a word while ignoring numbers") do
+    word = Word.new("1o2X4e5n")
+    expect(word.scrabble()).to eq(11)
   end
 end
