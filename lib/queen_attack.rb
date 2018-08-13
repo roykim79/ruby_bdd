68e6f15b0abf9b5a3833ff6f589f794623684813
Roy Kim <rykim79@gmail.com>
2018-08-13 10:48:20 -0400

    This commit will refactor the queen_attack method

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -4,15 +4,9 @@ class Array
     from_y = self[1]
     to_x = coordinates[0]
     to_y = coordinates[1]
+    diff_x = (from_x - to_x).abs
+    diff_y = (from_y - to_y).abs
 
-    if ((to_y - from_y) == 0) & ((to_x - from_x).abs > 0)
-      true
-    elsif ((to_x - from_x) == 0) & ((to_y- from_y).abs > 0)
-      true
-    elsif ((to_x - from_x).abs == (to_y- from_y).abs)
-      true
-    else
-      false
-    end
+    (diff_y.eql?(0) & !diff_x.eql?(0)) | (diff_x.eql?(0) & !diff_y.eql?(0)) | diff_x.eql?(diff_y)
   end
 end
